959c653af2a2686576a7e7207ee34f97c56f7db7
Sam Vargas <sam.vargas@gmx.com>
2019-11-18 16:29:02 +0000

    iteration2

--- starter-code/app.js
+++ starter-code/app.js
@@ -3,15 +3,23 @@
 const { join } = require('path');
 const express = require('express');
 const createError = require('http-errors');
-const cookieParser = require('cookie-parser');
+// const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const sassMiddleware = require('node-sass-middleware');
 const serveFavicon = require('serve-favicon');
 const hbs = require('hbs');
-
+const mongoose = require('mongoose');
 const indexRouter = require('./routes/index');
 
 const app = express();
+
+// Authentication logic
+const cookieParser = require('cookie-parser');
+const expressSession = require('express-session');
+const connectMongo = require('connect-mongo');
+
+const MongoStore = connectMongo(expressSession);
+
 
 // Setup view engine
 app.set('views', join(__dirname, 'views'));
@@ -21,7 +29,6 @@ hbs.registerPartials(join(__dirname, '/views/partials'));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(serveFavicon(join(__dirname, 'public/images', 'favicon.ico')));
 app.use(express.static(join(__dirname, 'public')));
 app.use(sassMiddleware({
@@ -31,12 +38,75 @@ app.use(sassMiddleware({
   sourceMap: true
 }));
 
+
+// Keeping sessions live with cookies
+app.use(cookieParser());
+
+app.use(
+  expressSession({
+    // We need to pass a secure session secret to express session
+    // in order for it to encrypt our password securely
+    // This secret should not be changed after it's set
+    secret: process.env.SESSION_SECRET,
+    // Resaving ensures that the cookie is refreshed every time the user makes a request
+    resave: true,
+    // Don't set a cookie if we haven't initialized a session
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 60 * 60 * 24 * 15,
+      // We should set this for security
+      // cookie.sameSite prevents CSRF attacks
+      sameSite: true,
+      // cookie.httpOnly means the cookie is not readable by JavaScript
+      httpOnly: true,
+      // We're setting cookie.secure to true on non-development environments only
+      // since in dev mode we're loading localhost throught http
+      secure: process.env.NODE_ENV !== 'development'
+    },
+    // Mongo store is going to save the value of req.session in a database record
+    // that will be loaded to req.session in every request
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 60 * 60 * 24
+    })
+  })
+);
+  
+
+// Deserializing user
+// Checks if there's a user ID saved on the session
+// If so, load the user from the database and bind it into req.user
+app.use((req, res, next) => {
+  const userId = req.session.user;
+  if (userId) {
+    User.findById(userId)
+      .then(user => {
+        req.user = user;
+        // Set the user in the response locals, so it can be accessed from any view
+        res.locals.user = req.user;
+        // Go to the next middleware/controller
+        next();
+      })
+      .catch(error => {
+        next(error);
+      });
+  } else {
+    // If there isn't a userId saved in the session,
+    // go to the next middleware/controller
+    next();
+  }
+});
+
+
+// Use one central router
 app.use('/', indexRouter);
+
 
 // Catch missing routes and forward to error handler
 app.use((req, res, next) => {
   next(createError(404));
 });
+
 
 // Catch all error handler
 app.use((error, req, res, next) => {
@@ -47,5 +117,6 @@ app.use((error, req, res, next) => {
   res.status(error.status || 500);
   res.render('error');
 });
+
 
 module.exports = app;
--- starter-code/routes/auth.js
+++ starter-code/routes/auth.js
@@ -40,4 +40,53 @@ router.post('/sign-up', (req, res, next) => {
 });
 
 
+//@GET    /auth/sign-in
+//@desc   get sing-in form
+//@access public
+router.get('/sign-in', (req, res, next) => {
+  res.render('sign-in');
+  // console.log(req.body);
+});
+
+
+//@POST   /auth/sign-in
+//@desc   Sign-in using post fomr
+//@access public
+router.post('/sign-in', (req, res, next) => {
+  let userId;
+  const { username, password } = req.body;
+  // Find a user with an email that corresponds to the one
+  // inserted by the user in the sign in form
+  User.findOne({ username })
+    .then(user => {
+      if (!user) {
+        // If no user was found, return a rejection with an error
+        // that will be sent to the error handler at the end of the promise chain
+        return Promise.reject(new Error("There's no user with that username."));
+      } else {
+        // If there is an user,
+        // save their ID to an auxiliary variable
+        userId = user._id;
+        console.log(userId);
+        // Compare the password with the salt + hash stored in the user document
+        return bcryptjs.compare(password, user.passwordHash);
+      }
+    })
+    .then(result => {
+      if (result) {
+        // If they match, the user has successfully been signed in
+        // req.session.user = userId;
+        console.log('Log in successful');
+        res.redirect('/');
+      } else {
+        // If they don't match, reject with an error message
+        return Promise.reject(new Error('Wrong password.'));
+      }
+    })
+    .catch(error => {
+      next(error);
+    });
+});
+
+
 module.exports = router;
